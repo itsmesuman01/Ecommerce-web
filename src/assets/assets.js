5517bf232af7faad17e8d1e5c723a98fbeeb8d01
fyp-preparation <sumanpandey977nep@gmail.com>
2024-08-14 07:49:52 +0545

    AssetsJS File Removed From GitIgnore

---
+++ src/assets/assets.js
@@ -0,0 +1,140 @@
+import logo from './images/logo.jpg'
+import search_icon from './images/search_icon.png'
+import basket_icon from './images/basket_icon.png'
+import header_img from './images/header_img.png'
+import shoes from './images/shoes.png'
+import clothes from './images/clothes.png'
+import electronics from './images/electronics.png'
+import rating_starts from './images/rating_starts.png'
+import add_icon_white from './images/add_icon_white.png'
+import remove_icon_red from './images/remove_icon_red.png'
+import add_icon_green from './images/add_icon_green.png'
+import facebook_icon from './images/facebook_icon.png'
+import linkedin_icon from './images/linkedin_icon.png'
+import twitter_icon from './images/twitter_icon.png'
+import play_store from './images/play_store.png'
+import app_store from './images/app_store.png'
+import cross_icon from './images/cross_icon.png'
+
+export const images = {
+    logo,
+    search_icon,
+    basket_icon,
+    header_img,
+    rating_starts,
+    add_icon_white,
+    remove_icon_red,
+    add_icon_green,
+    facebook_icon,
+    linkedin_icon,
+    twitter_icon,
+    play_store,
+    app_store,
+    cross_icon
+}
+
+export const product_list = [
+    {
+        product_name: "Shoes",
+        product_image: shoes
+    },
+    {
+        product_name: "Clothes",
+        product_image: clothes
+    },
+    {
+        product_name: "Electronics",
+        product_image: electronics
+    },
+    {
+        product_name: "Shoes",
+        product_image: shoes
+    },
+    {
+        product_name: "Clothes",
+        product_image: clothes
+    },
+    {
+        product_name: "Electronics",
+        product_image: electronics
+    },
+    {
+        product_name: "Shoes",
+        product_image: shoes
+    },
+    {
+        product_name: "Clothes",
+        product_image: clothes
+    },
+    {
+        product_name: "Electronics",
+        product_image: electronics
+    }
+]
+
+export const subproduct_list = [
+    {
+        id: 1,
+        name: 'Computer',
+        image: electronics,
+        price: 12,
+        description: 'asdddddddddddd',
+        category: 'Shoes'
+    },
+    {
+        id: 2,
+        name: 'Mobile',
+        image: electronics,
+        price: 13,
+        description: 'asdddddddddddd',
+        category: 'Shoes'
+    },
+    {
+        id: 3,
+        name: 'Tshirts',
+        image: clothes,
+        price: 14,
+        description: 'asdddddddddddd',
+        category: 'Clothes'
+    },
+    {
+        id: 4,
+        name: 'Paints',
+        image: clothes,
+        price: 15,
+        description: 'asdddddddddddd',
+        category: 'Clothes'
+    },
+    {
+        id: 5,
+        name: 'Computer',
+        image: electronics,
+        price: 12,
+        description: 'asdddddddddddd',
+        category: 'Electronics'
+    },
+    {
+        id: 6,
+        name: 'Mobile',
+        image: electronics,
+        price: 13,
+        description: 'asdddddddddddd',
+        category: 'Electronics'
+    },
+    {
+        id: 7,
+        name: 'Tshirts',
+        image: clothes,
+        price: 14,
+        description: 'asdddddddddddd',
+        category: 'Clothes'
+    },
+    {
+        id: 8,
+        name: 'Paints',
+        image: clothes,
+        price: 15,
+        description: 'asdddddddddddd',
+        category: 'Clothes'
+    }
+]
